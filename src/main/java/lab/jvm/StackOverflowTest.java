6d9314397ab0725a8dd119288737ea67709a30df
Artem Holubiev <artyom6500@gmail.com>
2026-09-03 13:31:19 +0300

    Fix StackOverflowTest

--- src/main/java/lab/jvm/StackOverflowTest.java
+++ src/main/java/lab/jvm/StackOverflowTest.java
@@ -1,4 +1,20 @@
 package lab.jvm;
 
 public class StackOverflowTest {
+
+    public static void recursion(int counter){
+        long long1 = 100000000;
+        long long2 = 100000000;
+        long long3 = 100000000;
+        long long4 = 100000000;
+        long long5 = 100000000;
+        counter++;
+        System.out.println("Current counter is: " + counter);
+        recursion(counter);
+    }
+
+    public static void main(String[] args){
+        recursion(0);
+
+    }
 }
